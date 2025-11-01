02d776c590e3eb1cf0f3845f8528583d776fdbf8
Serhat Karaman <serhatkaramanworkmail@gmail.com>
2025-11-01 18:57:56 +0300

    testing if on push working

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+
 def main():
+    print("###############################TESTING GITHUB ACTIONS ACTUALLY WORKING###############################")
+    print(f"This msg is committed on: {datetime.now()}")
     print("Hello from profile-readme-template!")
 
 
